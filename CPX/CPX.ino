7414500ae668d5bc0963d467c59a43990c899696
Carson Alberding <carson.alberdingg@gmail.com>
2024-10-18 13:29:33 -0400

    Create CPX.ino

---
+++ CPX/CPX.ino
@@ -0,0 +1,10 @@
+// https://github.com/adafruit/Adafruit_CircuitPlayground
+
+#include <Adafruit_CircuitPlayground.h>
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
